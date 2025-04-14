b0b44a513fafab790cb835909beffa2fff53e581
ERise <erise@4dsoft.hu>
2025-04-14 10:59:24 +0200

    dependencie, static code analysis, git-hooks

---
+++ git-hooks/pre-commit-windows.sh
@@ -0,0 +1,41 @@
+#!C:/Program\ Files/Git/usr/bin/sh.exe
+
+######## KTLINT-GRADLE HOOK START ########
+
+CHANGED_FILES="$(git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" && $2 ~ /\.kts|\.kt/ { print $2}')"
+
+if [ -z "$CHANGED_FILES" ]; then
+    echo "No Kotlin staged files."
+else
+    echo "Running ktlint over these files:"
+    echo "$CHANGED_FILES"
+
+    ./gradlew --quiet formatKotlin -PinternalKtlintGitFilter="$CHANGED_FILES"
+
+    echo "Completed ktlint run."
+
+    echo "$CHANGED_FILES" | while read -r file; do
+        if [ -f $file ]; then
+            git add $file
+        fi
+    done
+fi
+
+######## KTLINT-GRADLE HOOK END ########
+
+echo "Sorting dependencies."
+
+./gradlew sortDependencies
+
+echo "Completed sorting dependencies."
+
+# Look for any changed files that are gradle, gradle.kts, or toml and git add them.
+CHANGED_VERSION_FILES="$(git --no-pager diff --name-status --no-color --cached | awk '$1 != "D" && $2 ~ /\.gradle|\.toml|\.gradle.kts/ { print $2}')"
+
+echo "$CHANGED_VERSION_FILES" | while read -r file; do
+    if [ -f $file ]; then
+        git add $file
+    fi
+done
+
+echo "Completed pre-commit hook."

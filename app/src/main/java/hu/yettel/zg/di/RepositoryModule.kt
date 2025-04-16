751656ca8f1b0d8706d941257c9c3705ae722aa9
ERise <erise@4dsoft.hu>
2025-04-16 19:35:51 +0200

    separate repository to store and manipulate selected counties

--- app/src/main/java/hu/yettel/zg/di/RepositoryModule.kt
+++ app/src/main/java/hu/yettel/zg/di/RepositoryModule.kt
@@ -5,7 +5,9 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import hu.yettel.zg.data.repository.HighwayRepositoryImpl
+import hu.yettel.zg.data.repository.SelectedCountiesRepositoryImpl
 import hu.yettel.zg.domain.HighwayRepository
+import hu.yettel.zg.domain.SelectedCountiesRepository
 import javax.inject.Singleton
 
 @Module
@@ -14,4 +16,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindHighwayRepository(repository: HighwayRepositoryImpl): HighwayRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindSelectedCountiesRepository(repository: SelectedCountiesRepositoryImpl): SelectedCountiesRepository
 }

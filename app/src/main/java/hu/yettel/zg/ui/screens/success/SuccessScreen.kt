252b79012656d5e6c6e6ca9d5470903b54129f16
ERise <erise@4dsoft.hu>
2025-04-17 07:45:41 +0200

    refactored success screen to fit ui requirements regarding statusbar and navigationbar padding

--- app/src/main/java/hu/yettel/zg/ui/screens/success/SuccessScreen.kt
+++ app/src/main/java/hu/yettel/zg/ui/screens/success/SuccessScreen.kt
@@ -6,15 +6,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,64 +34,70 @@ import hu.yettel.zg.ui.designsystem.theme.Dimens
 import hu.yettel.zg.ui.designsystem.theme.Typography
 import hu.yettel.zg.ui.designsystem.theme.YettelZGTheme
 
+@Suppress("LongMethod")
 @Composable
 fun SuccessScreen(onDoneClick: () -> Unit) {
     BackHandler(enabled = true) {
         // No-op: Back press is intercepted and does nothing
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primary),
-    ) {
-        Image(
-            painter = painterResource(R.drawable.confetti),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.TopCenter),
-        )
-        Column(
+    Scaffold(
+        contentWindowInsets = WindowInsets.navigationBars,
+    ) { paddingValues ->
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.Bottom,
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(paddingValues),
         ) {
-            Text(
-                text = stringResource(R.string.success_screen_msg_lbl),
-                style = Typography.headlineLarge.copy(
-                    fontSize = 40.sp,
-                    lineHeight = 48.sp,
-                    color = MaterialTheme.colorScheme.secondary,
-                ),
-                modifier = Modifier
-                    .padding(
-                        Dimens.PaddingLarge,
-                    ),
-            )
-
             Image(
-                painter = painterResource(R.drawable.walking_man),
+                painter = painterResource(R.drawable.confetti),
                 contentDescription = null,
-                modifier = Modifier
-                    .heightIn(min = 293.dp)
-                    .widthIn(min = 281.dp)
-                    .align(Alignment.End)
-                    .offset(x = 40.dp),
-                contentScale = ContentScale.Fit,
-            )
-            // Bottom button
-            PrimaryButton(
-                text = stringResource(R.string.success_screen_done_btn),
-                onClick = onDoneClick,
+                contentScale = ContentScale.FillWidth,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(
-                        Dimens.PaddingLarge,
-                    ),
+                    .align(Alignment.TopCenter),
             )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Bottom,
+            ) {
+                Text(
+                    text = stringResource(R.string.success_screen_msg_lbl),
+                    style = Typography.headlineLarge.copy(
+                        fontSize = 40.sp,
+                        lineHeight = 48.sp,
+                        color = MaterialTheme.colorScheme.secondary,
+                    ),
+                    modifier = Modifier
+                        .padding(
+                            Dimens.PaddingLarge,
+                        ),
+                )
+
+                Image(
+                    painter = painterResource(R.drawable.walking_man),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .heightIn(min = 293.dp)
+                        .widthIn(min = 281.dp)
+                        .align(Alignment.End)
+                        .offset(x = 40.dp),
+                    contentScale = ContentScale.Fit,
+                )
+                // Bottom button
+                PrimaryButton(
+                    text = stringResource(R.string.success_screen_done_btn),
+                    onClick = onDoneClick,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            Dimens.PaddingLarge,
+                        ),
+                )
+            }
         }
     }
 }

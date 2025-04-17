d5936d0c8ad7ebcb2c2f1507496df98a54d57bff
ERise <erise@4dsoft.hu>
2025-04-17 07:45:03 +0200

    refactored application's topappbar to use built in material3 topappbar

--- app/src/main/java/hu/yettel/zg/ui/designsystem/components/YettelTopAppBar.kt
+++ app/src/main/java/hu/yettel/zg/ui/designsystem/components/YettelTopAppBar.kt
@@ -1,21 +1,19 @@
 package hu.yettel.zg.ui.designsystem.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,39 +22,49 @@ import hu.yettel.zg.ui.designsystem.theme.DarkBlue
 import hu.yettel.zg.ui.designsystem.theme.Dimens
 import hu.yettel.zg.ui.designsystem.theme.YettelZGTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun YettelTopAppBar(
     title: String,
     onBackClick: () -> Unit,
 ) {
-    Box(
+    TopAppBar(
         modifier = Modifier
-            .height(Dimens.AppBarHeight)
-            .fillMaxWidth()
-            .background(
-                color = MaterialTheme.colorScheme.primary,
-                shape = RoundedCornerShape(
+            .clip(
+                RoundedCornerShape(
                     bottomStart = Dimens.PaddingMedium,
                     bottomEnd = Dimens.PaddingMedium,
                 ),
-            ).padding(horizontal = Dimens.PaddingLarge, vertical = Dimens.PaddingSmall),
-        contentAlignment = Alignment.BottomStart,
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
+            ),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = DarkBlue,
+            navigationIconContentColor = DarkBlue,
+        ),
+        navigationIcon = {
             Icon(
                 painter = painterResource(id = R.drawable.back),
                 contentDescription = null,
-                tint = DarkBlue,
-                modifier = Modifier.clickable { onBackClick() },
+                modifier = Modifier
+                    .padding(
+                        start = Dimens.PaddingMedium,
+                        end = Dimens.PaddingExtraSmall,
+                    ).clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() },
+                    ) {
+                        onBackClick()
+                    },
             )
-            Spacer(Modifier.width(Dimens.PaddingExtraSmall))
+        },
+        title = {
             Text(
                 text = title,
                 style = MaterialTheme.typography.titleMedium,
                 color = DarkBlue,
             )
-        }
-    }
+        },
+    )
 }
 
 @Preview
